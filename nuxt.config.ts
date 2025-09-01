12c8804add47f8a061e827e570c247708809c1ad
Vishal Mule <advvish17@gmail.com>
2025-09-02 00:06:14 +0530

    prort changed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
+  devServer: {
+    port: 8000
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   vite: {
