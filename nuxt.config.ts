554274980997f4d6fa6a0bfda8bfc0d865841700
Vishal Mule <advvish17@gmail.com>
2025-09-02 00:11:56 +0530

    port for render.com

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,12 @@
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
+  nitro: {
+    preset: 'node-server', // ensure Node server build
+  },
   devServer: {
-    port: 8000
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3000
   },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
